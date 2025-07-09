379e930cf67232e34e4c45e54a6ac4fe49b9d3f2
Al King <alexander.king@canstar.com.au>
2025-07-09 21:36:52 +1000

    add stub to cmd

---
+++ cmd/tomlhander.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Activity struct {
+	Date    string `toml:"date"`
+	Page    int    `toml:"page"`
+	Minutes int    `toml:"minutes"`
+}
+
+type Book struct {
+	Title    string   `toml:"title"`
+	File     string   `toml:"file"`
+	Cover    string   `toml:"cover"`
+	Author   string   `toml:"author"`
+	Activity Activity `toml:"activity"`
+}
+
+type CacheToml struct {
+	CreatedAt string `toml:"created_at"`
+	UpdatedAt string `toml:"updated_at"`
+	Version   int    `toml:"version"`
+	Books     []Book `toml:"books"`
+}
+
+func ReadToml(path string) CacheToml {
+	fmt.Print("Target Path", path)
+	doc, readErr := os.ReadFile(path)
+	if readErr != nil {
+		fmt.Print("File Read Error", readErr)
+		var empty CacheToml
+		return empty
+	}
+	var db CacheToml
+	err := toml.Unmarshal([]byte(doc), &db)
+	if err != nil {
+		fmt.Print("Toml Read Error", err)
+		var empty CacheToml
+		return empty
+	}
+	return db
+}
+
+func SaveToml(db CacheToml, path string) bool {
+	b, err := toml.Marshal(db)
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile(path, b, 0644)
+	if err != nil {
+		fmt.Print("File Write Error", err)
+		return false
+	}
+	return true
+}
