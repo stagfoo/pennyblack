fa9807080f0a65239486bb98b036080e92da5949
Al King <alexander.king@canstar.com.au>
2025-07-09 21:28:32 +1000

    update deps

---
+++ get-public-books.sh
@@ -0,0 +1,6 @@
+!#bin/bash
+
+cd mock/books
+wget -O comic-1.cbr https://archive.org/download/AdventuresIntoTheUnknown1-112/Adventures_Into_The_Unknown_001_1948.cbr
+sleep 1
+wget -O book.epub https://www.gutenberg.org/ebooks/8128.epub.images
